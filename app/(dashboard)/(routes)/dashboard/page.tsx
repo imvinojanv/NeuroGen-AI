36aad30344adec8e962704abeaa6093dc7200d0a
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-07-21 02:27:18 +0530

    Clerk Auth 🛡️🆗

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from '@clerk/nextjs';
 
 const DashboardPage = () => {
   return (
     <>
       <h1 className="text-3xl">Dashboard Page (Protected)</h1>
-      <Button variant="destructive" size="lg">Hello there</Button>
+      <UserButton afterSignOutUrl="/" />
     </>
   )
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,23 @@
+import { Button } from "@/components/ui/button";
+import Link from 'next/link';
+
 const LandingPage = () => {
   return (
-    <>
+    <div>
       <h1 className="text-3xl">Landing Page (Unprotected)</h1>
-    </>
+      <div>
+        <Link href="/sign-in">
+          <Button>
+            Login
+          </Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>
+            Register
+          </Button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
