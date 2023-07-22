6f731575c3e85a51a5d7ef849f4d43165f344344
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-07-22 09:52:45 +0530

    Test with debug ⚗️

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,8 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
-    publicRoutes: ["/"]     // Unprotected landing page
+    publicRoutes: ["/"],     // Unprotected landing page
+    // debug: true
 });
 
 export const config = {
