c865fce5e82efd07da4470a7ded8b8c2ed48603a
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-07-22 19:35:54 +0530

    Dashboard done 🔣✅

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { Montserrat } from 'next/font/google';
+import { usePathname } from 'next/navigation';
 import { 
     LayoutDashboard, 
     MessageSquare,
@@ -66,6 +67,8 @@ const routes = [
 ]
 
 function Sidebar() {
+    const pathname = usePathname();
+
   return (
     <div className='space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white'>
         <div className='px-3 py-2 flex-1'>
@@ -86,7 +89,11 @@ function Sidebar() {
                     <Link
                         href={route.href}
                         key={route.href}
-                        className='text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition'
+                        className={cn(
+                            'text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition',
+                            // for the active page section
+                            pathname === route.href ? 'text-white bg-white/10' : 'text-zinc-400'
+                        )}
                     >
                         <div className='flex items-center flex-1'>
                             <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
