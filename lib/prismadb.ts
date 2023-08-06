321375060ece816e145e5c0e052ef165d2d58c81
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2023-08-07 00:35:41 +0530

    Updated Prisma

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -8,4 +8,12 @@ declare global {
 const prismadb = globalThis.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;     // for the development
 
+// prismadb.$connect()
+//     .then(() => console.log("Prisma client connected to the database"))
+//     .catch((error) => {
+//         console.error("Error connecting to the database:");
+//         console.error(error);
+//         process.exit(1);
+//     });
+
 export default prismadb;
